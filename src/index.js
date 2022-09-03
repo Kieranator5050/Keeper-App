fa7422ae69fd5283489ea77175a902912d9303a2
Kieran Jaggernauth <thejugg63@gmail.com>
2022-09-03 15:26:47 -0500

    disabled eslint warning for styles

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
+// eslint-disable-next-line no-unused-vars
 import styles from "./styles.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
